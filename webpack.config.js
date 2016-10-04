0fcbc7055080599fd1e8e4dae8303ad29ada4be0
Mario Gomez <mariogomezmtz@gmail.com>
2016-10-04 16:56:04 -0500

    Update loaders and required dependecies

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['./app/app']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'app'),
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        include: path.join(__dirname,'app/scss'),
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  }
+}
